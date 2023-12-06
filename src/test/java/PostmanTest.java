72513f8b2f2b0e91de889ec696ac53c60e5a0416
Nata test <verba16@yandex.ru>
2023-12-06 20:34:03 +0300

    initial commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void ShouldReturnBody() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("1976")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("1976"))
+        ;
+    }
+}
